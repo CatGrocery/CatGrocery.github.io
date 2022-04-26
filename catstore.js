49e93262d784805bf16ac0674f4470c6c639e79e
CatGrocery <101174916+CatGrocery@users.noreply.github.com>
2022-04-26 20:53:46 +0800

    v3

--- catstore.js
+++ catstore.js
@@ -2,6 +2,7 @@ var ShowHintIteam = [];
 var IsMoible;
 var NowCanva;
 var NowBuyFilters;
+var ModalIteam = [];
 
 
 function showHint(str)
@@ -40,22 +41,24 @@ function showHint(str)
                         {
                                 var result=xmlhttp.responseText;
                                 var obj = JSON.parse(result);//解析json字串為json物件形式
-                                                        
                                 var html = '';
                                 
                             switch(str)
                             {
                                 case('寶石'):
                                 {
+                                html  +='<ul class="filters-bar nav nav-center nav-bold nav-uppercase nav-pills" data-shuffle="filter"><li class="nav-item pl-1"><a class="nav-link active" href="#石榴石" >石榴石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#黃寶石" >黃寶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#綠寶石" >綠寶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#藍寶石" >藍寶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#冒險之星" >冒險之星</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#紫水晶" >紫水晶</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#騎士寶石" >騎士寶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#玻隕石" >玻隕石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#錫石" >錫石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#黑玉" >黑玉</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#菫青石" >菫青石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#深藍寶石" >深藍寶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#尖晶石" >尖晶石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#賽黃晶" >賽黃晶</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#黃水晶" >黃水晶</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#錳紅柱石" >錳紅柱石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#橄欖石" >橄欖石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#石英" >石英</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#珍珠" >珍珠</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#蛋白石" >蛋白石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#貓眼石" >貓眼石</a></li><li class="nav-item pl-1"><a class="nav-link active" href="#紅寶石" >紅寶石</a></li></ul><br><br><br>';
                                 for (var i = 1; i < obj.length; i ++ ) 
-                                    {//
-                                        html  +='<div class="row justify-content-center pb-4" data-shuffle="item" data-groups="pasta"><div class="col-lg-2 mx-auto text-left"><a  class="product-media" style="display: flex; justify-content: center;"><img loading="lazy" class="lazyload " style="width:100%; max-height: 200px; object-fit: contain; object-position: center " src="image/'+obj[i].data[1]+'" alt=""></a></div>';
-                                        html  += '<div class="col-xs-12  col-lg-4 mx-auto"><h3> <a style="color: #fbbc04" >' + obj[i].data[0] + '</a></h3>';//
+                                    {
+                                        html  +='<div id="' +  obj[i].data[8] + '">';
+                                        html  +='<div class="row justify-content-center pb-4" data-shuffle="item" data-groups="pasta"><div class="col-lg-2 mx-auto text-left"><a  class="product-media" style="display: flex; justify-content: center;"><img loading="lazy" class="lazyload " style="width:100%; max-height: 200px; object-fit: contain; object-position: center " src="image/'+obj[i].data[1]+'" alt=""></a></div></ul>';
+                                        html  += '<div class="col-xs-12  col-lg-4 mx-auto"><h3> <a style="color: #fbbc04" >' + obj[i].data[0] + '</a></h3>';
                                         html  +='<div class="extra-fields-all"> <div class="extra-field" style="background-color: ; color: ; font-size: 14px; padding-left: 0px ;"><b class="extra-field-column-name">價格: </b> <span class="extra-field-value">'+obj[i].data[4]+ '<span>';
                                         html  +='</div> <div class="extra-field" style="background-color: ; color: ; font-size: 14px; padding-left: 0px ;"><b class="extra-field-column-name">武器效果: </b> <span class="extra-field-value">'+obj[i].data[5]+'<span>';
                                         html  +='</div> <div class="extra-field" style="background-color: ; color: ; font-size: 14px; padding-left: 0px ;"><b class="extra-field-column-name">防具效果: </b> <span class="extra-field-value">'+obj[i].data[3]+'<span>';
                                         html  +='</div> <div class="extra-field" style="background-color: ; color: ; font-size: 14px; padding-left: 0px ;"><b class="extra-field-column-name">現貨: </b> <span class="extra-field-value">'+obj[i].data[6]+'<span>'+'</div></div>';
                                         html  +='<div style="background-color: #e9ecf0; color: #3a3a3a;; margin-right: 5px;" class="badge">'+obj[i].data[7]+'</div></div>';
+                                        
                                         if(obj[i].data[2] == "立即購買")
                                         {
                                             html  +='<div class="col-xs-12 col-lg-2 mx-auto"><a target="_blank" class="product-media" style="display: flex; justify-content: center;" ><button class="btn w-100" data-toggle="modal" data-target="#myModal" onclick="ShowModal(this)" style="background-color: #fbbc04; color: #ffffff;" id="'+obj[i].data[9]+'">'+obj[i].data[2]+'</button></a></div></div><hr>';
@@ -65,6 +68,7 @@ function showHint(str)
                                             html  +='<div class="col-xs-12 col-lg-2 mx-auto"><a target="_blank" class="product-media" style="display: flex; justify-content: center;"><button class="btn w-100" style="background-color: #fbbc04; color: #ffffff;"> '+obj[i].data[2]+'</button></a></div></div><hr>';
                                         }  
     
+                                        html  +='</div>';
                                         ShowHintIteamData = [];
                                                 for(j=0 ; j < obj[i].data.length ; j++)
                                                 {
@@ -275,10 +279,11 @@ function ShowAnnouncement()
         xmlhttp.send();
 }
 
-
+//Modal顯示內容更換
 function ShowModal(btn)
 {
     //document.getElementById('intocart-title').innerHTML='超級大漢堡';
+    document.getElementById('addcartbtn').style.pointerEvents = "none" ;
     var BtnId = btn.id;
     for(i=1 ; i<ShowHintIteam.length ; i++)
     {
@@ -293,7 +298,16 @@ function ShowModal(btn)
                 document.getElementById("modal-remark-gem").innerHTML = "<b>武器效果 :</b>"  +ShowHintIteam[i][5] +"<br><b>防具效果 :</b>"  +ShowHintIteam[i][5] ;
                 document.getElementById("modal-remark").innerHTML ="";
                 document.getElementById("modal-spotgoods").innerHTML = ShowHintIteam[i][6];
+                document.getElementById("oder-num").value = 0 ;
+                document.getElementById("Subtotal").innerHTML = 0;
+               for(j = 0 ; j < ShowHintIteam[i].length ; j++)
+               {
+                   ModalIteam[j] = ShowHintIteam[i][j];
+               }
+               
             }
+
+
             else
             {
                 document.getElementById("modal-title").innerHTML = ShowHintIteam[i][0];
@@ -302,14 +316,22 @@ function ShowModal(btn)
                 document.getElementById("modal-price").innerHTML = ShowHintIteam[i][4];
                 document.getElementById("modal-remark").innerHTML = ShowHintIteam[i][5];
                 document.getElementById("modal-spotgoods").innerHTML = ShowHintIteam[i][6];
+                document.getElementById("oder-num").value = 0 ;
+                document.getElementById("Subtotal").innerHTML = 0;
+                for(j = 0 ; j < ShowHintIteam[i].length ; j++)
+                {
+                    ModalIteam[j] = ShowHintIteam[i][j];
+                }
+
             }
             
-        
         }
     }
     
 }
 
+
+//側邊選單開啟/關閉
 var sidecanvas;
 function openNav() {
     if(sidecanvas)
@@ -377,6 +399,8 @@ function openNav() {
     }
     window.addEventListener('message', adjustModalTop, false);
 
+
+    //判斷是否是手機
     function mobile()
     {
         try{
@@ -394,6 +418,8 @@ function openNav() {
             IsMoible = false;
     }
 
+
+    //側邊選單選取後，更改顯示文字
     function ClickSideNavBtn(self)
     {
         var text = self.value;
@@ -401,6 +427,7 @@ function openNav() {
     }  
 
 
+    //網頁載入時，執行的funtion
     function webload()
     {
         ShowAnnouncement();
@@ -417,6 +444,7 @@ function openNav() {
         //document.getElementById('buy').style.display= "none";
     }
 
+    //更換顯示中主頁
     function maincanvaChoose(self)
     {
         if(NowCanva != self.id)
@@ -430,6 +458,90 @@ function openNav() {
             document.getElementById(showCanva).style.display= "";
             NowCanva = self.id ;
             //alert(NowCanva);
+        } 
+
+        if(IsMoible)
+        {
+           if(sidecanvas)
+           {
+            openNav();
+            document.getElementById('nav-toggle').checked = false;
+           }
         }
         
     }
+
+    //數量+-按鈕
+    function calculate(cal_type,cul_target,num)
+    {
+
+        switch (cal_type)
+        {
+            case 'add':
+            {
+                var nownum;
+                nownum = parseInt(document.getElementById(cul_target).value) ;
+                nownum += num; 
+                document.getElementById(cul_target).value = nownum;
+                Subtotal('Subtotal');
+                break;
+            }
+
+            case 'reduce':
+            {
+                
+                var nownum;
+                nownum = parseInt(document.getElementById(cul_target).value) ;
+                if(nownum <= num)
+                {
+                    nownum = 0 ; 
+                }
+                else
+                {
+                    nownum -= num; 
+                }
+                document.getElementById(cul_target).value = nownum;
+                Subtotal('Subtotal');
+                break;
+            }  
+            
+            
+        }
+
+        if(document.getElementById(cul_target).value == 0)
+        {
+            document.getElementById('addcartbtn').style.pointerEvents = "none" ;
+        }
+        else
+        {
+            document.getElementById('addcartbtn').style.pointerEvents = "auto" ;
+        }
+    }
+
+    function Subtotal(textID)
+    {
+        var price = ModalIteam[4].split(" ");
+        document.getElementById(textID).innerHTML = ( document.getElementById('oder-num').value* price[0]).toString();
+    }
+    
+    function addcart()
+    {
+        if(document.getElementById('oder-num').value <= 0)
+        {
+            alert('請輸入商品數量');
+        }
+        else
+        {
+            var addcookie = '';
+            for(i=0 ; i<ModalIteam.length-1 ; i++)
+            {
+                addcookie += ModalIteam[i].toString() + ",";
+            }
+            addcookie += document.getElementById('oder-num').value ;
+            document.cookie = ModalIteam[9] + '=' +  addcookie ;
+            
+        }
+        
+        
+    }
+
